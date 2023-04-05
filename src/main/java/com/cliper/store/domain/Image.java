698efceacef5452bab4b212dda91db2fa564cde6
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-04-05 15:56:28 +0900

    feat: Image 도메인 구현

---
+++ src/main/java/com/cliper/store/domain/Image.java
@@ -0,0 +1,32 @@
+package com.cliper.store.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity(name = "IMAGE")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Inheritance(strategy = InheritanceType.JOINED)
+@DynamicInsert
+public class Image extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private int imageId;
+
+    @Column(name = "file_name")
+    private String fileName;
+
+    @Column(name = "is_thumbnail")
+    private boolean isThumbnail;
+
+    @Builder
+    public Image(int imageId, String fileName, boolean isThumbnail) {
+        this.imageId = imageId;
+        this.fileName = fileName;
+        this.isThumbnail = isThumbnail;
+    }
+}
