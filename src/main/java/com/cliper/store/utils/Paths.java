0425fa68e91ae107a25ea432704e3655321f43f4
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-04-11 01:41:15 +0900

    chore: path 수정

--- src/main/java/com/cliper/store/utils/Paths.java
+++ src/main/java/com/cliper/store/utils/Paths.java
@@ -2,7 +2,7 @@ package com.cliper.store.utils;
 
 public class Paths {
 
-    public static final String IMAGE_PATH = "file:///C:/Users/user/Desktop/clipper/img/";
-    public static final String RESOURCE_PATH = "C:/Users/user/Desktop/clipper/img/";
+    public static final String IMAGE_PATH = "file:///var/www/ClipperBack/img/";
+    public static final String RESOURCE_PATH = "/var/www/ClipperBack/img";
 
 }
